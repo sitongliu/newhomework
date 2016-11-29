4488aed6377d91592dafefc7f83368dc9b6ceeea
LiuSitong <1018708788@qq.com>
2016-11-29 13:03:58 +0800

    StringBuffer和StringBuilder的比较 以及方法的应用 掌握字符串拼接的方法

---
+++ src/com/liu/test20161128/B.java
@@ -0,0 +1,17 @@
+package com.liu.test20161128;
+
+/**
+ * Created by LiuSitong on 2016/11/29.
+ */
+public class B {
+    //StringBuffer StringBuilder
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Hello World").append("  Hello World");
+        sb.length();
+        System.out.println(sb.length());
+    }
+
+
+}
